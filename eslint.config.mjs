4651b4cb9ff7814f5b7a93fb6b7e219f7cb1d722
Joseph Kastier <joseph.kastier@finanzen-zero.net>
2025-12-07 16:41:09 +0100

    try to fix ci

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,10 +5,10 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist', '**/out-tsc'],
+    ignores: ['**/dist', '**/out-tsc', '**/*.config.*'],
   },
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    files: ['apps/**/*.ts', 'apps/**/*.tsx', 'apps/**/*.js', 'apps/**/*.jsx', 'libs/**/*.ts', 'libs/**/*.tsx', 'libs/**/*.js', 'libs/**/*.jsx'],
     rules: {
       '@nx/enforce-module-boundaries': [
         'error',
@@ -27,14 +27,22 @@ export default [
   },
   {
     files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.cts',
-      '**/*.mts',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
+      'apps/**/*.ts',
+      'apps/**/*.tsx',
+      'apps/**/*.cts',
+      'apps/**/*.mts',
+      'apps/**/*.js',
+      'apps/**/*.jsx',
+      'apps/**/*.cjs',
+      'apps/**/*.mjs',
+      'libs/**/*.ts',
+      'libs/**/*.tsx',
+      'libs/**/*.cts',
+      'libs/**/*.mts',
+      'libs/**/*.js',
+      'libs/**/*.jsx',
+      'libs/**/*.cjs',
+      'libs/**/*.mjs',
     ],
     // Override or add rules here
     rules: {},
