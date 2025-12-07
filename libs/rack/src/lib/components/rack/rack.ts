4d0242ab5c46d1960f1775d4e0b6d21de0ac915b
Joseph Kastier <joseph.kastier@finanzen-zero.net>
2025-12-07 16:26:18 +0100

    setup rack component

--- libs/rack/src/lib/components/rack/rack.ts
+++ libs/rack/src/lib/components/rack/rack.ts
@@ -1,16 +1,28 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { BeerFacade } from '../../facades/beer.facade';
+import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { Beer } from '../../models/beer.model';
 
 @Component({
   selector: 'lib-rack',
-  imports: [],
+  imports: [FooterComponent, CommonModule],
   templateUrl: './rack.html',
   styleUrl: './rack.scss',
 })
 export class Rack implements OnInit {
   protected readonly beerFacade = inject(BeerFacade);
+  protected selectedBeer = signal<Beer | null>(null);
 
   ngOnInit(): void {
     this.beerFacade.loadBeers();
+  }
+
+  onBeerClick(beer: Beer): void {
+    this.selectedBeer.set(beer);
+  }
+
+  closeBeerToast(): void {
+    this.selectedBeer.set(null);
   }
 }
