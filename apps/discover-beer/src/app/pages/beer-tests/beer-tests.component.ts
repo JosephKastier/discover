079e10186eddedff89750e6aedfe7978a396d86e
Joseph Kastier <joseph.kastier@finanzen-zero.net>
2025-12-09 15:40:07 +0100

    some refactoring

--- apps/discover-beer/src/app/pages/beer-tests/beer-tests.component.ts
+++ apps/discover-beer/src/app/pages/beer-tests/beer-tests.component.ts
@@ -2,12 +2,11 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BeerTestStore } from '../../store/beer-test.store';
 import { BeerTestCardComponent } from '../../components/beer-test-card/beer-test-card.component';
-import { HeroComponent } from '../../components/hero/hero.component';
 
 @Component({
   selector: 'app-beer-tests',
   standalone: true,
-  imports: [CommonModule, BeerTestCardComponent, HeroComponent],
+  imports: [CommonModule, BeerTestCardComponent],
   templateUrl: './beer-tests.component.html',
   styleUrl: './beer-tests.component.scss',
 })
